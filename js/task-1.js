651b0bf42cb7b60f31ceff373889824d03358a1c
AnastAnt <141515437+AnastAnt@users.noreply.github.com>
2023-11-15 21:06:09 +0100

    /

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function makeTransaction(quantity, pricePerDroid, customerCredits){
+   let infoPrise;
+    const totalPrise = quantity * pricePerDroid;
+    
+    if (customerCredits > totalPrise) {
+        infoPrise = `Insufficient funds!`;
+            } else {
+        infoPrise = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+    return (infoPrise);
+}
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
